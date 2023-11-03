90fb1e0d2cfeba3fa32da0af1fadc13bca124e00
jbarrod529 <145765219+jbarrod529@users.noreply.github.com>
2023-11-03 12:21:47 +0100

    Update tabla_multiplicar.php

--- tabla_multiplicar.php
+++ tabla_multiplicar.php
@@ -4,12 +4,12 @@
 $numeroMultiplicar = $_POST['numeroMultiplicar'];
 $multiplicion = 0;
 
-for (i = 0; i <= 10; i++){
+for ($i = 1; $i <= 10; $i++){
 
     $multiplicacion = $numeroMultiplicar * $i;
     
 
-    echo $numeroMultiplicar . " X " . $i . " = " . $multiplicacion .;
+    echo $numeroMultiplicar . " X " . $i . " = " . $multiplicacion ."<br>";
 }
 
 
